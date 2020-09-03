bd0e3e7a121ce856efeb45d44d529c3886766dc1
marcusungarette <marcusungarette@gmail.com>
2020-09-03 10:11:48 -0300

    Added Mutation

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,13 +4,26 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = resolve(__dirname, 'schema.graphql')
 
 const USERS = [
-    { id: 1, name: 'Joao da Silva' },
-    { id: 2, name: 'Maria de Souza' }
+    { id: 1, name: 'Joao da Silva', email: 'joaodasilva@gmail.com' },
+    { id: 2, name: 'Maria de Souza', email: 'mariadesouza@gmail.com' }
 ]
 
 const resolvers = {
     Query: {
         users: () => USERS
+    },
+
+    Mutation: {
+        createUser: (parent, args, ctx, info) => {
+            console.log('args:', args)
+            const { data } = args
+            const user = {
+                ...data,
+                id: USERS.length + 1
+            }
+            USERS.push(user)
+            return user
+        }
     }
 }
 
