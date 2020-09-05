fdf4ed2cef8c6994676aedb6a86d94880934668b
marcusungarette <marcusungarette@gmail.com>
2020-09-05 12:35:05 -0300

    Added Linter and Prettier

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,10 @@
 import server from './server'
 
-const { PORT: port =  4001 } = process.env
+const { PORT: port = 4001 } = process.env
 
 const main = async (): Promise<void> => {
-    await server.start({ port })
-    console.log(`Listening at port ${port}...`)
+  await server.start({ port })
+  console.log(`Listening at port ${port}...`)
 }
 
 main().catch(console.log)
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,39 +4,39 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = resolve(__dirname, 'schema.graphql')
 
 const USERS = [
-    { id: 1, name: 'Joao da Silva', email: 'joaodasilva@gmail.com' },
-    { id: 2, name: 'Maria de Souza', email: 'mariadesouza@gmail.com' }
+  { id: 1, name: 'Joao da Silva', email: 'joaodasilva@gmail.com' },
+  { id: 2, name: 'Maria de Souza', email: 'mariadesouza@gmail.com' },
 ]
 
 const resolvers = {
-    User: {
-        name: (parent) => {
-            console.log('Parent:', parent)
-            return 'User: ' + parent.name
-        }
+  User: {
+    name: (parent): string => {
+      console.log('Parent:', parent)
+      return 'User: ' + parent.name
     },
+  },
 
-    Query: {
-        users: () => USERS
+  Query: {
+    users: (): typeof USERS => USERS,
+  },
+
+  Mutation: {
+    createUser: (parent, args): typeof USERS[0] => {
+      console.log('args:', args)
+      const { data } = args
+      const user = {
+        ...data,
+        id: USERS.length + 1,
+      }
+      USERS.push(user)
+      return user
     },
-
-    Mutation: {
-        createUser: (parent, args, ctx, info) => {
-            console.log('args:', args)
-            const { data } = args
-            const user = {
-                ...data,
-                id: USERS.length + 1
-            }
-            USERS.push(user)
-            return user
-        }
-    }
+  },
 }
 
 const server = new GraphQLServer({
-    resolvers,
-    typeDefs,
+  resolvers,
+  typeDefs,
 })
 
 export default server
